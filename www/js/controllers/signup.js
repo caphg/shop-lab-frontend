e4b38d596d6d8c8d54bc354696bc0346dff12e3e
Hrvoje G <hrvoje.grdic1@gmail.com>
2016-04-02 21:32:47 +0200

    register form email

--- www/js/controllers/signup.js
+++ www/js/controllers/signup.js
@@ -1,9 +1,12 @@
 APP.controller('SignupCtrl', function($scope, $auth, State) {
+
+  $scope.registrationForm = {email: '', password: '', password_confirmation: ''};
+
   $scope.handleRegBtnClick = function() {
     $auth.submitRegistration($scope.registrationForm)
       .then(function(resp) {
         // handle success response
-        State.showInfo("Confirmation email has been sent to address you specified.");
+        State.showInfo("Confirmation email has been sent to " + $scope.registrationForm.email);
         State.navigate("/signin");
       })
       .catch(function(resp) {
