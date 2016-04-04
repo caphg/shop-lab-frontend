dd98a6af4a4c951d4c404b0e7b05060a34104d12
Hrvoje G <hrvoje.grdic1@gmail.com>
2016-04-04 08:45:41 +0200

    deploy

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 var express = require('express'),
 app = express();
 app.use(express.static('www'));
-app.use('/privacy', express.static(__dirname + '/public/www/js/templates/privacy.html'));
+app.use('/privacy', express.static(__dirname + '/www/templates/privacy.html'));
 app.set('port', process.env.PORT || 5000);
 app.listen(app.get('port'), function () {
     console.log('Express server listening on port ' + app.get('port'));
